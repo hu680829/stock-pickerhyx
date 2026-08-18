954f836d04eeba4db29ccab541d92beb29f1487a
hu680829 <ft6zt65h86@privaterelay.appleid.com>
2026-08-19 06:43:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 
 url = "http://www.pushplus.plus/send"
 data = {
-    "token": "你的Token",
+    "token": "5758507929d44bf8b067c06781bcbe84",
     "title": "手机测试",
     "content": "这是一条手机网页端测试消息",
 }
